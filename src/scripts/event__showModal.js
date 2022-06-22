bec17fc936f33a1e2061c2300221d2e29fa725b1
Jon Wright <dr.wrong.mo@gmail.com>
2022-06-21 22:24:33 -0500

    Added delete function

--- src/scripts/event__showModal.js
+++ src/scripts/event__showModal.js
@@ -1,8 +1,57 @@
 const deleteModal = document.getElementById('deleteConfirm');
+let data = JSON.parse(localStorage.getItem('FEM-comments'));
 
-function showDeleteModal (e) {
-    if (e.target.classList.contains('dlt')) {
-        deleteModal.showModal()
-    }
+class Comment {
+  constructor(id, user, content, score, createdAt) {
+    this.id = id;
+    this.user = user;
+    this.content = content;
+    this.replies = [];
+    this.score = score
+    this.createdAt = createdAt
+  }
 }
-module.exports = showDeleteModal
+
+function removeObjectByID(id) {
+  let newData = [];
+  Array.from(data.comments).forEach((el) => {
+    if (el.id !== id) {
+      let newEl = new Comment(el.id, el.user, el.content, el.score, el.createdAt);
+      newData.push(newEl);
+
+      el.replies.forEach((subEl) => {
+        if (subEl.id !== id) {
+          newData[newData.length - 1].replies.push(subEl);
+        }
+      });
+    }
+  });
+
+  data.comments = newData;
+  localStorage.setItem('FEM-comments', JSON.stringify(data));
+}
+
+function showDeleteModal(e) {
+  const parent = e.target.closest('.dlt');
+
+  // Return if click occurred outside of delete button group
+  if (parent === null) {
+    return null;
+  }
+
+  deleteModal.showModal();
+
+  let deleteButton =
+    deleteModal.lastElementChild.lastElementChild.lastElementChild;
+  let grandparent =
+    e.target.parentElement.parentElement.parentElement.parentElement;
+
+  let id = Number(grandparent.getAttribute('id').replace('commentID-', ''));
+
+  deleteButton.addEventListener('click', () => {
+    grandparent.remove();
+    removeObjectByID(id);
+    localStorage.setItem('FEM-comments', JSON.stringify(data));
+  });
+}
+module.exports = showDeleteModal;
